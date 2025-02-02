9bcd38e72b545f6b1bf424984c3be0670bba610b
Jyothi Buddha <buddhaj@amazon.com>
2025-02-02 11:15:21 -0800

    feat: ui components to create, filter, sort and show posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CreatePost } from './components/CreatePost.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostList } from './components/PostList.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+const posts = [
+  { title: 'Learning Redux', contents: 'State management', author: 'Jyothi Buddha', tags: ['redux'] },
+  { title: 'Learning React Hooks', author: 'Reshma', tags: ['react', 'hooks'] },
+  { title: 'Learning Full Stack React', author: 'Rakesh', tags: ['react', 'nodejs'] },
+  { title: 'Guide to typescript' },
+]
 
+export function App() {
+  // return <Post title='Full-Stack React Projects' contents="Let's become full-stack developers" author='Daniel Bugl' />
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React + Node.js</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
   )
 }
-
-export default App
