8c6b5e90bc46275e2939da1e1d204ee0e254f157
Jyothi Buddha <buddhaj@amazon.com>
2025-02-02 12:49:35 -0800

    feat: working blog app with mutability and data fetched from mongodb

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import { CreatePost } from './components/CreatePost.jsx'
-import { PostFilter } from './components/PostFilter.jsx'
-import { PostList } from './components/PostList.jsx'
-import { PostSorting } from './components/PostSorting.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Blog } from './Blog.jsx'
 
-const posts = [
-  { title: 'Learning Redux', contents: 'State management', author: 'Jyothi Buddha', tags: ['redux'] },
-  { title: 'Learning React Hooks', author: 'Reshma', tags: ['react', 'hooks'] },
-  { title: 'Learning Full Stack React', author: 'Rakesh', tags: ['react', 'nodejs'] },
-  { title: 'Guide to typescript' },
-]
+const queryClient = new QueryClient()
 
 export function App() {
-  // return <Post title='Full-Stack React Projects' contents="Let's become full-stack developers" author='Daniel Bugl' />
   return (
-    <div style={{ padding: 8 }}>
-      <CreatePost />
-      <br />
-      <hr />
-      Filter by:
-      <PostFilter field='author' />
-      <br />
-      <PostSorting fields={['createdAt', 'updatedAt']} />
-      <hr />
-      <PostList posts={posts} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Blog></Blog>
+    </QueryClientProvider>
   )
 }
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,73 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
+import { createPost } from '../api/posts'
+
 export function CreatePost() {
+  const [author, setAuthor] = useState('')
+  const [title, setTitle] = useState('')
+  const [contents, setContents] = useState('')
+
+  const queryClient = useQueryClient()
+
+  const createPostMutation = useMutation({
+    mutationFn: () => createPost({ title, author, contents }),
+    onSuccess: () => {
+      setAuthor('')
+      setTitle('')
+      setContents('')
+      queryClient.invalidateQueries(['posts'])
+    },
+  })
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        createPostMutation.mutate()
+      }}
+    >
       <div>
         <label htmlFor='create-title'>Title: </label>
-        <input type='text' name='create-title' id='create-title' />
+        <input
+          type='text'
+          name='create-title'
+          id='create-title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          disabled={createPostMutation.isPending}
+        />
       </div>
       <br />
       <div>
         <label htmlFor='create-author'>Author: </label>
-        <input type='text' name='create-author' id='create-author' />
+        <input
+          type='text'
+          name='create-author'
+          id='create-author'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          disabled={createPostMutation.isPending}
+        />
       </div>
       <br />
-      <textarea />
+      <textarea
+        value={contents}
+        onChange={(e) => setContents(e.target.value)}
+        disabled={createPostMutation.isPending}
+      />
       <br />
       <br />
-      <input type='submit' value='Create' />
+      <input
+        type='submit'
+        value={createPostMutation.isPending ? 'Creating...' : 'Create'}
+        disabled={!title || createPostMutation.isPending}
+      />
+      {createPostMutation.isSuccess ? (
+        <>
+          <br />
+          Post created successfully
+        </>
+      ) : null}
     </form>
   )
 }
