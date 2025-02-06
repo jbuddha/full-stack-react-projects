262cff4311021adb6f91e2b94e2ac1d39d0f1d59
Jyothi Buddha <buddhaj@amazon.com>
2025-02-05 23:14:32 -0800

    feat: user signup

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import { postsRoutes } from './routes/posts.js'
+import { userRoutes } from './routes/users.js'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 postsRoutes(app)
+userRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!!!')
