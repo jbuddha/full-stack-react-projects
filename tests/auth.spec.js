81bf513362aa839cb305124c0a97a361a8884852
Jyothi Buddha <buddhaj@amazon.com>
2025-05-04 09:44:12 -0700

    chore: renaming test file to avoid ts error

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,7 +1,6 @@
-import { test, expect } from './fixtures/index.js'
+import { test } from './fixtures/index.js'
 
 test('allows signup and login', async ({ page, auth }) => {
-  await page.goto('/')
-  const testUser = await auth.signUpAndLogin()
+  await auth.signUpAndLogin()
   await page.getByRole('button', { name: 'Logout' }).click()
 })
